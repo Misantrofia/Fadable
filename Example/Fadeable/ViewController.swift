fdec5778844994abe5eab6d4dc3718ba33b6cea6
Alexandru schwartz <alexandru.schwartz@softvision.ro>
2016-03-12 22:46:21 +0200

    Initial commit

---
+++ Example/Fadeable/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Fadeable
+//
+//  Created by Alexandru schwartz on 03/12/2016.
+//  Copyright (c) 2016 Alexandru schwartz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
